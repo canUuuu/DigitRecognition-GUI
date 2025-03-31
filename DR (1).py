c54b6de91f09588581c6348911241a1b6ca0ab24
sbr <2919291481@qq.com>
2025-03-31 23:04:19 +0800

    model

--- DR (1).py
+++ DR (1).py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from utils import *
 import os
 import numpy as np
@@ -14,5 +15,12 @@ mnist = tf.keras.datasets.mnist
 # normalization to [0-1]
 x_train , x_test = imageNormalization(x_train, x_test)
 
-# checkMNISTdata(x_train, y_train, x_test, y_test)
+checkMNISTdata(x_train, y_train, x_test, y_test)
 # checkMATPLOTLIB(x_train[0], y_train[0])
+
+model = Sequential()
+
+# x_train.shape:[60000, 28, 28, channel = 1]
+model.add(Conv2D(64, (3, 3), input_shape = x_train.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2, 2)))
