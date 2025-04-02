b34becfadc3ba37d0a867efa995fc51a32d21ceb
sbr <2919291481@qq.com>
2025-04-02 19:46:17 +0800

    增加了GUI文件生成图形界面

---
+++ GUI.py
@@ -0,0 +1,47 @@
+from utils import *
+
+
+try:
+    while True:
+        # get all events
+        e = pygame.event.wait()
+        draw_partition_line()
+
+        # clear screen after right click
+        if(e.type == pygame.MOUSEBUTTONDOWN and e.button == 3):
+            screen.fill(white)
+
+        # quit
+        if e.type == pygame.QUIT:
+            raise StopIteration
+
+        # start drawing after left click
+        if(e.type == pygame.MOUSEBUTTONDOWN and e.button != 3):
+            color = black
+            pygame.draw.circle(screen, color, e.pos, radius)
+            draw_on = True
+
+        # stop drawing after releasing left click
+        if e.type == pygame.MOUSEBUTTONUP and e.button != 3:
+            draw_on = False
+            fname = "ASSETS/out.png"
+
+            img = crope(screen)
+            pygame.image.save(img, fname)
+
+            output_img = get_output_image(fname)
+            show_output_image(output_img)
+
+        # start drawing line on screen if draw is true
+        if e.type == pygame.MOUSEMOTION:
+            if draw_on:
+                pygame.draw.circle(screen, color, e.pos, radius)
+                roundline(screen, color, e.pos, last_pos, radius)
+            last_pos = e.pos
+
+        pygame.display.flip()
+
+except StopIteration:
+    pass
+
+pygame.quit()
